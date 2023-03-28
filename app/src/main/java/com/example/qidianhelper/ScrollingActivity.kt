5e877d029f5a641972809c81529bb8757c65e54c
Shuocheng Wang <wangshch5@outlook.com>
2023-03-28 18:46:16 +0800

    Change icons and texts

--- app/src/main/java/com/example/qidianhelper/ScrollingActivity.kt
+++ app/src/main/java/com/example/qidianhelper/ScrollingActivity.kt
@@ -3,12 +3,8 @@ package com.example.qidianhelper
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
-import com.google.android.material.appbar.CollapsingToolbarLayout
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import com.example.qidianhelper.databinding.ActivityScrollingBinding
 
